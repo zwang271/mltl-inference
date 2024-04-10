e8b70dae1c555e6c83df26606ec9e8388fc3a7fb
Luke Marzen <98473324+lmarzen@users.noreply.github.com>
2024-04-09 22:22:09 -0500

    astar: added timer

--- src/astar/main.cpp
+++ src/astar/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
+#include <sys/time.h>
 
 #include "evaluate_mltl.h"
-
 
 int main(int argc, char *argv[]) {
   // default options
@@ -20,8 +20,21 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  bool val = evaluate_mltl("G[0,3]p1", {"01","11","01","11"}, true);
+  struct timeval start, end;
+  double time_taken = 0;
+  gettimeofday(&start, NULL); // start timer
+
+  bool val = evaluate_mltl("G[0,3]p1", {"01", "11", "01", "11"}, false);
   std::cout << val << "\n";
+
+  gettimeofday(&end, NULL); // stop timer
+  time_taken = end.tv_sec + end.tv_usec / 1e6 - start.tv_sec -
+               start.tv_usec / 1e6; // in seconds
+
+  // std::cout << "total nodes generated: " << numNodes << "\n";
+  std::cout << "total time taken: " << time_taken << "s\n";
+  // std::cout << "path length: " << path.length() << "\n";
+  // std::cout << "path: " << path << "\n";
 
   return 0;
 }
