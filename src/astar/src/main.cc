d93e79620ab225def7619d3222540d8e55fcea21
Luke Marzen <98473324+lmarzen@users.noreply.github.com>
2024-04-21 02:45:15 -0500

    astar: wip, working on generating boolean functions

--- src/astar/src/main.cc
+++ src/astar/src/main.cc
@@ -3,11 +3,13 @@
 #include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <set>
 #include <sys/time.h>
 
 // #include "evaluate_mltl.h"
 #include "parser.hh"
 #include "quine_mccluskey.hh"
+#include <boost/container/flat_set.hpp>
 
 using namespace std;
 using namespace libmltl;
@@ -19,6 +21,13 @@ size_t max_trace_length(const vector<vector<string>> &traces) {
   }
   return max;
 }
+
+struct ASTNodeUniquePtrCompare {
+  bool operator()(const std::unique_ptr<ASTNode> &lhs,
+                  const std::unique_ptr<ASTNode> &rhs) const {
+    return *lhs < *rhs; // Use the ASTNode comparison operators
+  }
+};
 
 int main(int argc, char *argv[]) {
   // default options
@@ -37,7 +46,7 @@ int main(int argc, char *argv[]) {
     }
   }
 
-  string base_path = "../dataset/basic_global";
+  string base_path = "../dataset/nasa-atc_formula2";
   vector<vector<string>> traces_pos_train =
       read_trace_files(base_path + "/pos_train");
   size_t max_pos_train_trace_len = max_trace_length(traces_pos_train);
@@ -55,35 +64,43 @@ int main(int argc, char *argv[]) {
   double time_taken = 0;
   gettimeofday(&start, NULL); // start timer
 
-  const uint32_t num_vars = 2;
-  const uint32_t truth_table_rows = pow(2, num_vars);
-  const uint64_t num_boolean_functions = pow(2, pow(2, num_vars));
+  const size_t num_vars = traces_pos_train[0][0].length();
+  const size_t truth_table_rows = pow(2, num_vars);
+  const size_t num_boolean_functions = pow(2, pow(2, num_vars));
   vector<string> inputs(truth_table_rows);
-  vector<unique_ptr<Globally>> boolean_functions_asts(num_boolean_functions);
-  vector<unique_ptr<Until>> boolean_functions_asts_until(num_boolean_functions);
-  vector<string> boolean_functions_asts_string(num_boolean_functions);
-  vector<string> boolean_functions_asts_until_string(num_boolean_functions);
+  // vector<unique_ptr<Globally>> boolean_functions_asts(num_boolean_functions);
+  // vector<unique_ptr<Globally>> boolean_functions_asts;
+  set<unique_ptr<ASTNode>, ASTNodeUniquePtrCompare> boolean_functions_asts;
 
   for (uint32_t i = 0; i < truth_table_rows; ++i) {
     inputs[i] = int_to_bin_str(i, num_vars);
   }
 
   // #pragma omp parallel for num_threads(12)
-  for (uint64_t i = 0; i < num_boolean_functions; ++i) {
-    vector<string> implicants;
-    for (uint32_t j = 0; j < truth_table_rows; ++j) {
-      if ((i >> j) & 1) {
-        implicants.emplace_back(inputs[j]);
+  gettimeofday(&start, NULL); // start timer
+
+  for (size_t i = 0; i < traces_pos_train.size(); ++i) {
+    for (size_t end = 0; end < traces_pos_train[i].size(); ++end) {
+      for (size_t start = 0; start <= end; ++start) {
+        boost::container::flat_set<string> implicantsset;
+        for (size_t j = start; j <= end; ++j) {
+          implicantsset.emplace(traces_pos_train[i][j]);
+        }
+        vector<string> implicants(implicantsset.begin(), implicantsset.end());
+        boolean_functions_asts.emplace(quine_mccluskey(implicants));
       }
     }
-    cout << quine_mccluskey(implicants)->as_string() << "\n";
-    cout << quine_mccluskey_fast_string(implicants) << "\n";
-    boolean_functions_asts[i] =
-        make_unique<Globally>(quine_mccluskey(implicants), 0, 10);
-    boolean_functions_asts_string[i] = boolean_functions_asts[i]->as_string();
   }
 
-  // #pragma omp parallel for num_threads(12)
+  gettimeofday(&end, NULL); // stop timer
+  time_taken = end.tv_sec + end.tv_usec / 1e6 - start.tv_sec -
+               start.tv_usec / 1e6; // in seconds
+
+  for (auto &formula : boolean_functions_asts) {
+    cout << formula->as_string() << "\n";
+  }
+
+  /* ENUMERATE ALL BOOLEAN FUNCTIONS
   for (uint64_t i = 0; i < num_boolean_functions; ++i) {
     vector<string> implicants;
     for (uint32_t j = 0; j < truth_table_rows; ++j) {
@@ -91,11 +108,13 @@ int main(int argc, char *argv[]) {
         implicants.emplace_back(inputs[j]);
       }
     }
-    boolean_functions_asts_until[i] = make_unique<Until>(
-        quine_mccluskey(implicants), quine_mccluskey(implicants), 0, 10);
-    boolean_functions_asts_until_string[i] =
-        boolean_functions_asts_until[i]->as_string();
+    // cout << quine_mccluskey(implicants)->as_string() << "\n";
+    boolean_functions_asts.emplace_back(
+        make_unique<Globally>(quine_mccluskey(implicants), 0, 10));
+    // boolean_functions_asts_string[i] =
+  boolean_functions_asts[i]->as_string();
   }
+  */
 
   // #pragma omp parallel for
   // for (int i = 0; i < num_boolean_functions; ++i) {
@@ -104,70 +123,61 @@ int main(int argc, char *argv[]) {
   //         evaluate_mltl(formula, {"0101", "1101", "0101", "1101"}, false);
   // }
 
-  gettimeofday(&start, NULL); // start timer
+  /*
+  #pragma omp parallel for num_threads(1)
+    for (uint64_t i = 0; i < num_boolean_functions; ++i) {
+      // dynamic_cast<MLTLUnaryTempOpNode>(boolean_functions_asts[i])->child;
+      for (int ub = 0; ub < max_pos_train_trace_len; ++ub) {
+        for (int lb = 0; lb <= ub; ++lb) {
+          if (ub - lb < 8) {
+            continue;
+          }
+          int traces_satisified = 0;
+          boolean_functions_asts[i]->set_lower_bound(lb);
+          boolean_functions_asts[i]->set_upper_bound(ub);
+          for (size_t j = 0; j < traces_pos_train.size(); ++j) {
+            traces_satisified +=
+                boolean_functions_asts[i]->evaluate(traces_pos_train[j]);
+          }
+          for (size_t j = 0; j < traces_neg_train.size(); ++j) {
+            traces_satisified +=
+                !boolean_functions_asts[i]->evaluate(traces_neg_train[j]);
+          }
 
-#pragma omp parallel for num_threads(1)
-  for (uint64_t i = 0; i < num_boolean_functions; ++i) {
-    // dynamic_cast<MLTLUnaryTempOpNode>(boolean_functions_asts[i])->child;
-    for (int ub = 0; ub < max_pos_train_trace_len; ++ub) {
-      for (int lb = 0; lb <= ub; ++lb) {
-        if (ub - lb < 8) {
-          continue;
-        }
-        int traces_satisified = 0;
-        boolean_functions_asts[i]->set_lower_bound(lb);
-        boolean_functions_asts[i]->set_upper_bound(ub);
-        for (size_t j = 0; j < traces_pos_train.size(); ++j) {
-          traces_satisified +=
-              boolean_functions_asts[i]->evaluate(traces_pos_train[j]);
-          // unique_ptr<ASTNode> test =
-          // parse("(((((((((((p0&p0&p0&p0&p0&p0&p0&p0&p0&p0&p0&p0&p0&p0&~p1))))R[0,10](p2)))))R[0,10](p2))))");
-          // unique_ptr<ASTNode> test2 =
-          // parse("(((((((((((p0&p0&p0&p0&p0&p0&p0&p0&p0&p0&p0&p0&p0&p0&~p1))))R[0,10](p2)))))R[0,10](p2))))");
-          // unique_ptr<ASTNode> test3 =
-          // parse("(((((((((((p0->p0->p0->p0->p0->p0->p0->p0^~p1))))R[0,10](p2)))))U[0,10](p2))))");
-          // unique_ptr<ASTNode> test4 =
-          // parse("(((((((((((((((((((((p0->(((((((p0->p0)->p0))))))->p0->p0->p0->p0^~p1))))))))))))))R[0,10](p2)))))U[0,10](p2))))");
-        }
-        for (size_t j = 0; j < traces_neg_train.size(); ++j) {
-          traces_satisified +=
-              !boolean_functions_asts[i]->evaluate(traces_neg_train[j]);
-        }
-
-        float accuracy = traces_satisified / (float)(traces_pos_train.size() +
-                                                     traces_neg_train.size());
-        if (accuracy >= 0) {
-          // cout << boolean_functions_asts[i]->as_string() << "\n";
-          // cout << "accuracy: " << accuracy << "\n";
+          float accuracy = traces_satisified / (float)(traces_pos_train.size() +
+                                                       traces_neg_train.size());
+          if (accuracy >= 0) {
+            // cout << boolean_functions_asts[i]->as_string() << "\n";
+            // cout << "accuracy: " << accuracy << "\n";
+          }
         }
       }
     }
-  }
-
+  */
   // gettimeofday(&start, NULL); // start timer
 
-  for (uint64_t i = 0; i < num_boolean_functions; ++i) {
-    // MLTLNode *new_node = parse(boolean_functions_asts_string[i]);
-    for (size_t j = 0; j < traces_pos_train.size(); ++j) {
-      unique_ptr<ASTNode> new_node = parse(boolean_functions_asts_string[i]);
-      new_node->evaluate(traces_pos_train[j]);
-      // cout << new_node->as_string() << "\n";
-    }
-    for (size_t j = 0; j < traces_neg_train.size(); ++j) {
-      unique_ptr<ASTNode> new_node = parse(boolean_functions_asts_string[i]);
-      new_node->evaluate(traces_neg_train[j]);
-    }
-  }
-  for (uint64_t i = 0; i < num_boolean_functions; ++i) {
-    unique_ptr<ASTNode> new_node =
-        parse(boolean_functions_asts_until_string[i]);
-    for (size_t j = 0; j < traces_pos_train.size(); ++j) {
-      new_node->evaluate(traces_pos_train[j]);
-    }
-    for (size_t j = 0; j < traces_neg_train.size(); ++j) {
-      new_node->evaluate(traces_neg_train[j]);
-    }
-  }
+  // for (uint64_t i = 0; i < num_boolean_functions; ++i) {
+  //   // MLTLNode *new_node = parse(boolean_functions_asts_string[i]);
+  //   for (size_t j = 0; j < traces_pos_train.size(); ++j) {
+  //     unique_ptr<ASTNode> new_node = parse(boolean_functions_asts_string[i]);
+  //     new_node->evaluate(traces_pos_train[j]);
+  //     // cout << new_node->as_string() << "\n";
+  //   }
+  //   for (size_t j = 0; j < traces_neg_train.size(); ++j) {
+  //     unique_ptr<ASTNode> new_node = parse(boolean_functions_asts_string[i]);
+  //     new_node->evaluate(traces_neg_train[j]);
+  //   }
+  // }
+  // for (uint64_t i = 0; i < num_boolean_functions; ++i) {
+  //   unique_ptr<ASTNode> new_node =
+  //       parse(boolean_functions_asts_until_string[i]);
+  //   for (size_t j = 0; j < traces_pos_train.size(); ++j) {
+  //     new_node->evaluate(traces_pos_train[j]);
+  //   }
+  //   for (size_t j = 0; j < traces_neg_train.size(); ++j) {
+  //     new_node->evaluate(traces_neg_train[j]);
+  //   }
+  // }
 
   // gettimeofday(&end, NULL); // stop timer
   // time_taken = end.tv_sec + end.tv_usec / 1e6 - start.tv_sec -
@@ -194,9 +204,6 @@ int main(int argc, char *argv[]) {
   //   }
   // }
 
-  gettimeofday(&end, NULL); // stop timer
-  time_taken = end.tv_sec + end.tv_usec / 1e6 - start.tv_sec -
-               start.tv_usec / 1e6; // in seconds
   cout << "total time taken: " << time_taken << "s\n";
 
   // for (int i = 0; i < num_boolean_functions; ++i) {
@@ -206,58 +213,6 @@ int main(int argc, char *argv[]) {
   // cout << "total nodes generated: " << numNodes << "\n";
   // cout << "path length: " << path.length() << "\n";
   // cout << "path: " << path << "\n";
-
-  unique_ptr<ASTNode> test = parse("true");
-  cout << test->as_string() << "\n";
-  test = parse("false");
-  cout << test->as_string() << "\n";
-  test = parse("(true)");
-  cout << test->as_string() << "\n";
-  test = parse("~(false)");
-  cout << test->as_string() << "\n";
-  // test = parse("~fale");
-  test = parse("G[0,1]true");
-  cout << test->as_string() << "\n";
-  test = parse("F[0,1]!false");
-  cout << test->as_string() << "\n";
-  // test = parse("G[10,0]false");
-  test = parse("~falseU[0,10]!false");
-  cout << test->as_string() << "\n";
-  // test = parse("(falseR[0,10](!false))");
-  // test = parse("(falsasdR[0,10](!false))");
-  // test = parse("(falseR[0,10](!falsasdfe))");
-  // cout << test->as_string() << "\n";
-  test = parse("false&true");
-  cout << test->as_string() << "\n";
-  test = parse("((false&false)R[0,10](!false))");
-  cout << test->as_string() << "\n";
-  test = parse("((false&false|true)R[0,10](!false))");
-  cout << test->as_string() << "\n";
-  test = parse("~(true)&true");
-  cout << test->as_string() << "\n";
-  test = parse("G[0,4](true)&true");
-  cout << test->as_string() << "\n";
-
-  test = parse("((false&false&true^G[0,4]true&~true)R[0,10](!false))");
-  cout << test->as_string() << "\n";
-  test = parse("((false&false&true^G[0,4](true)&~true)R[0,10](!false))");
-  cout << test->as_string() << "\n";
-  test = parse("((false&false&true^(G[0,4](true))&~true)R[0,10](!false))");
-  cout << test->as_string() << "\n";
-  // ((false&false&true^G[0,4](true)&~true)R[0,10](!false))
-  test = parse("~(true)&true");
-  cout << test->as_string() << "\n";
-  test = parse("G[0,4](true)&true");
-  cout << test->as_string() << "\n";
-  // test = parse("G[0,1](p&true");
-  // cout << test->as_string() << "\n";
-  test = parse("(p0&~p1)R[1,4](p2)");
-  // test = parse("(false)R[0,3](!false)");
-  cout << test->as_string() << "\n";
-  cout << test->evaluate(
-              {"000", "001", "001", "101", "000"})
-  // "000", "101", "100", "110"})
-       << "\n";
 
   return 0;
 }
