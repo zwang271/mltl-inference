272346b04893f3a8d51ece5b8aa35dc307a3c344
Luke Marzen <98473324+lmarzen@users.noreply.github.com>
2024-04-20 20:44:28 -0500

    astar: restructured to use libmltl submodule

--- src/astar/quine_mccluskey.hh
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "libmltl/ast.hh"
-
-using namespace std;
-
-string int_to_bin_str(unsigned int n, int width);
-string quine_mccluskey_fast_string(const vector<string> &implicants);
-unique_ptr<ASTNode> quine_mccluskey(const vector<string> &implicants);
