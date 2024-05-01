da0946855f3e551504980e8b71b345b9c43dc794
swamjay <44168659+swamjay@users.noreply.github.com>
2024-05-01 10:07:58 -0500

    Revert "Merge branch 'main' of https://github.com/zwang271/mltl-inference"
    This reverts commit 503e568b448746157db96853611d8b2c1db93de7, reversing
    changes made to e69db0d0a9cc90e17e487757f1ea23e2dcc15ebe.

--- src/BN/structure_learning.py
+++ src/BN/structure_learning.py
@@ -1,12 +1,17 @@
 import bnlearn as bn
 import pandas as pd
 import matplotlib.pyplot as plt
+import pathlib
 from utils import *
 
 if __name__ == "__main__":
-    df = pd.read_csv("data.txt")
-    model = bn.structure_learning.fit(df)
-    print(model['adjmat'])
-    G = bn.plot(model)
+    dataset = pathlib.Path("../dataset/fmsd17_formula1/")
+    pos_train = load_traces(dataset / "pos_train/")
+    neg_train = load_traces(dataset / "neg_train/")
+    pos_test = load_traces(dataset / "pos_test/")   
+    neg_test = load_traces(dataset / "neg_test/")
+
+    for trace in neg_train:
+        print(len(trace))
 
 
